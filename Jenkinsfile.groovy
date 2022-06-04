d829a6c7cabf4592598d200741d6808d8b46fbe9
tq <2310307125@qq.com>
2022-06-04 10:53:56 +0800

    ff

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline{
         body: "<p>Executed: Job(<b>'${env.JOB_NAME}:${env.BUILD_NUMBER}'</b>)</p><p>View console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME}:${env.BUILD_NUMBER}</a></p><p><i>()Build log is attached.</i></p>", 
         compressLog: true,
         attachLog: true,
-        recipientProviders: [culprint(),developers(),requestors(),brokenBuildSuspects()],
+        recipientProviders: [culprits(),developers(),requestors(),brokenBuildSuspects()],
         to: '2310307125@qq.com'
         )
       }
