447b5bf2848c226e1b206755bc085b307d54bbdd
tq <2310307125@qq.com>
2022-06-04 10:51:37 +0800

    ff

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,10 +16,16 @@ pipeline{
 
   post{
       failure{
-        emailext compressLog: true,
+        emailext( 
+        subject: "Status: ${currentBuild.result} - Job:'${env.JOB_NAME}'",
+        body: "<p>Executed: Job(<b>'${env.JOB_NAME}:${env.BUILD_NUMBER}'</b>)</p>
+        <p>View console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME}:${env.BUILD_NUMBER}</a></p>
+        <p><i>()Build log is attached.</i></p>", 
+        compressLog: true,
         attachLog: true,
         recipientProviders: [culprint(),developers(),requestors(),brokenBuildSuspects()],
         to: '2310307125@qq.com'
+        )
       }
   }
 }
