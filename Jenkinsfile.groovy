4e9852c5a9fce364e88df08f9006db9971c161b9
tq <2310307125@qq.com>
2022-06-04 10:41:14 +0800

    ff

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,9 +7,19 @@ pipeline{
         steps{
           ansiblePlaybook(
             playbook: "${env.WORKSPACE}/playbook.yml",
-            inventory: "${env.WORKSPACE}/hosts"
+            inventory: "${env.WORKSPACE}/hosts",
+
           )
         }
+      }
+  }
+
+  post{
+      failure{
+        emailext compresslog:true,
+        attachLog: true,
+        recipientProviders: [culprint(),developers(),requestors(),brokenBuildSuspects()],
+        to: '2310307125@qq.com'
       }
   }
 }
